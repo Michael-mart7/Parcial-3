54dfa1e66a458688455d47fdf1714ae4b607da6b
unknown <maicolmart7@gmail.com>
2025-06-06 18:01:13 -0500

    configuracio para comprobar que sirva

--- models/db.js
+++ models/db.js
@@ -1,10 +1,20 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,        // ejemplo: o1xyz.mysql.render.com
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT, // importante para filess.io
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+    if (err) {
+    console.error('❌ Error de conexión a la base de datos:', err);
+    } else {
+    console.log('✅ Conexión exitosa a la base de datos');
+    }
 });
 
 module.exports = connection;
